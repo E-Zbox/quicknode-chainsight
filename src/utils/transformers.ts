bf0f92642041d2b101a95624729767b9a0fe6618
E-Zbox <zboxstudios97@gmail.com>
2024-06-29 20:03:31 +0100

    helper functions

---
+++ src/utils/transformers.ts
@@ -0,0 +1,15 @@
+export const convertTimeInMsToWords = (timestamp: number) => {
+  const timeDiff = (Date.now() - timestamp) / 1000;
+
+  if (timeDiff < 60) {
+    return `${timeDiff} secs ago`;
+  } else if (timeDiff < 3600) {
+    let result = timeDiff / 60;
+    return `${result.toFixed(0)} mins ago`;
+  } else if (timeDiff < 86400) {
+    let result = timeDiff / 3600;
+    return `${result.toFixed(0)} hours ago`;
+  }
+  let result = timeDiff / 86400;
+  return `${result.toFixed(0)} days ago`;
+};
